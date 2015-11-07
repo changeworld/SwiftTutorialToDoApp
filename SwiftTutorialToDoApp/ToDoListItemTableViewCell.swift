22ce0d47f680c8ace8dccf108dbebe92d0b8113d
changeworld <changesworlds@gmail.com>
2015-11-07 16:34:57 +0900

    Modify Main.storyboard
    Add Table View, Table View Cell
    Add ToDoListItemTableViewCell.swift

---
+++ SwiftTutorialToDoApp/ToDoListItemTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ToDoListItemTableViewCell.swift
+//  SwiftTutorialToDoApp
+//
+//  Created by 竹林崇 on 2015/11/07.
+//  Copyright (c) 2015年 takashi.takebayashi. All rights reserved.
+//
+
+import UIKit
+
+class ToDoListItemTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
